4f2e5231786f3ee54953ddf73b829a957d6639c1
lecook <laura.emily.cook14@gmail.com>
2022-03-14 14:51:34 +1100

    update

--- code/whole_genome_alignment/lastz.sh
+++ code/whole_genome_alignment/lastz.sh
@@ -4,7 +4,7 @@
 ## generates a separate command for each scaffold
 ## These commands are then used in a slurm array script to run jobs in parallel
 
-TRA=($(for file in *.chain; do echo $file |cut -d "." -f 1-2;done))
+TRA=($(for file in *.maf; do echo $file |cut -d "." -f 1-2;done))
 
 echo ${TRA[@]}
 
@@ -13,14 +13,16 @@ for tr in ${TRA[@]};
 do
 
 # generate lastz commands
-echo 'lastz_32 /data/projects/punim0586/lecook/chipseq-pipeline/cross_species/data/genomes/mm10.fa[multiple] /data/projects/punim0586/lecook/chipseq-pipeline/cross_species/data/genomes/smiCra1_RM/'${tr}.fa 'H=2000 K=2400 L=3000 Y=9400 --format=maf > /data/projects/punim0586/lecook/chipseq-pipeline/cross_species/data/genomes/maf/'${tr}_mm10.smiCra1.maf
+#echo 'lastz_32 /data/projects/punim0586/lecook/chipseq-pipeline/cross_species/data/genomes/mm10.fa[multiple] /data/projects/punim0586/lecook/chipseq-pipeline/cross_species/data/genomes/smiCra1_RM/'${tr}.fa 'H=2000 K=2400 L=3000 Y=3400 --scores=/data/projects/punim0586/lecook/chipseq-pipeline/cross_species/bin/GenomeAlignmentTools/HoxD55.q --format=maf > /data/projects/punim0586/lecook/chipseq-pipeline/cross_species/data/genomes/maf/'${tr}_mm10.smiCra1.maf
+
+
 
 # generate convert maf to axt format commands
-echo 'maf-convert axt '${tr}.maf' > '${tr}.axt
+echo 'maf-convert psl '${tr}.maf' > '${tr}.psl
 
 ## build co-linear alignment chains
-echo 'axtChain -linearGap=loose /data/projects/punim0586/lecook/chipseq-pipeline/cross_species/data/genomes/axt/'${tr}'.axt /data/projects/punim0586/lecook/chipseq-pipeline/cross_species/data/genomes/mm10.2bit /data/projects/punim0586/lecook/chipseq-pipeline/cross_species/data/genomes/smiCra1.2bit '${tr}'.chain'
+#echo 'axtChain -linearGap=loose -scoreScheme=../../bin/GenomeAlignmentTools/HoxD55.q '${tr}'.axt /data/projects/punim0586/lecook/chipseq-pipeline/cross_species/data/genomes/mm10.2bit /data/projects/punim0586/lecook/chipseq-pipeline/cross_species/data/genomes/smiCra1.2bit '${tr}'.chain'
 
-echo 'chainSort '${tr}'.chain > '${tr}'.sorted.chain'
+#echo 'chainSort '${tr}'.chain > '${tr}'.sorted.chain'
 
 done
