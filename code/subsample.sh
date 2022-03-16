3528c30894149a96e5903eb885c14ee82f5ce6b3
lecook <lecook@student.unimelb.edu.au>
2022-03-16 14:29:41 +1100

    fixed filenames for mouse

--- code/subsample.sh
+++ code/subsample.sh
@@ -9,8 +9,8 @@ for tr in ${TRA[@]};
 do
 echo ${tr}
 
-#frac=$( samtools idxstats ${tr}_q30.sorted.dedup.bam | cut -f3 | awk 'BEGIN {total=0} {total += $1} END {frac=10000000/total; if (frac > 1) {print 1} else {print frac}}' )
+frac=$( samtools idxstats ${tr}_q30.sorted.dedup.bam | cut -f3 | awk 'BEGIN {total=0} {total += $1} END {frac=10000000/total; if (frac > 1) {print 1} else {print frac}}' )
 
-#samtools view -bs $frac ${tr}_PPq30.sorted.dedup.bam > ${tr}_downSampled.PPq30.sorted.dedup.bam
+samtools view -bs $frac ${tr}_q30.sorted.dedup.bam > ${tr}_downSampled_q30.sorted.dedup.bam
 
 done
